265ee7ae4db148393083bc9c14fdf07def7d2ac7
jclizar <jessica_lizar@yahoo.com>
2024-08-21 09:43:05 -0300

    fix relative path CREATE + add table emprestimos + consultas e atualiazação file

---
+++ atualizacao_exclusao.py
@@ -0,0 +1,15 @@
+'''
+4. Atualizações e Exclusões:
+- Escreva consultas SQL para atualizar e excluir registros do banco de
+dados, por exemplo, para marcar um livro como devolvido ou remover
+um autor.
+'''
+
+import conexao as conn
+
+# Atualizar | Jessica
+
+# Excluir | Nadi
+
+conn.conexao.commit()
+conn.conexao.close()
---
+++ consultas.py
@@ -0,0 +1,18 @@
+'''
+3. Consultas SQL:
+Escreva consultas SQL para realizar as seguintes operações:
+'''
+import conexao as conn
+
+# Listar todos os livros disponíveis. | raquel
+
+# Encontrar todos os livros emprestados no momento. | Livia
+
+# Localizar os livros escritos por um autor específico. | Leticia
+
+# Verificar o número de cópias disponíveis de um determinado livro. | Jéssica
+
+# Mostrar os empréstimos em atraso. | Rosana
+
+conn.conexao.commit()
+conn.conexao.close()
