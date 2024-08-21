072321f2e5e7ad8dff96781c8c2a8c39e221d7a2
let <leticiacostaoa@gmail.com>
2024-08-21 19:52:14 -0300

    fazendo a consulta SQL para autores

--- consultas.py
+++ consultas.py
@@ -13,6 +13,14 @@ for livros in list_books:
 # Encontrar todos os livros emprestados no momento. | Livia
 
 # Localizar os livros escritos por um autor específico. | Leticia
+join_result = conn.cursor.execute('''
+                                    SELECT * 
+                                    FROM livros 
+                                    JOIN autores_livros ON livros.id_livro = autores_livros.id_livro 
+                                    JOIN pessoas ON pessoas.id_pessoa = autores_livros.id_pessoa 
+                                    WHERE pessoas.nome = "Nilo Ney Coutinho";
+                                ''')
+print(join_result)
 
 # Verificar o número de cópias disponíveis de um determinado livro. | Jéssica
 
