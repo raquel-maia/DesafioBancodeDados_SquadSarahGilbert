393d7768d85bbbc7ebed14a946bc939d113ac325
nadiduno <nadiduno@gmail.com>
2024-08-22 11:27:12 -0300

    Excluir autor

--- atualizacao_exclusao.py
+++ atualizacao_exclusao.py
@@ -10,6 +10,20 @@ import conexao as conn
 # Atualizar | Jessica+
 
 # Excluir | Nadi
+#Excluir tabelas
+# conn.cursor.execute('DROP TABLE livros_generos')
+# conn.cursor.execute('DROP TABLE autores_livros')
+# conn.cursor.execute('DROP TABLE livros')
+# conn.cursor.execute('DROP TABLE generos')
+# conn.cursor.execute('DROP TABLE exemplares')
+# conn.cursor.execute('DROP TABLE pessoas')
+# conn.cursor.execute('DROP TABLE usuarios')
+# conn.cursor.execute('DROP TABLE autores')
+# conn.cursor.execute('DROP TABLE ')
+
+#Excluir um autor
+id_autor = 7
+conn.cursor.execute('DELETE FROM autores WHERE id = id_autor')
 
 conn.conexao.commit()
 conn.conexao.close()
